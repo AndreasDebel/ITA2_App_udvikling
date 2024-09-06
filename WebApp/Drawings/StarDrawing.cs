1aac99f83102b75bb48522227cce51d6a8dd9319
AndreasDebel <andreasdebel@hotmail.com>
2024-09-06 14:33:12 +0200

    crazy star

--- WebApp/Drawings/StarDrawing.cs
+++ WebApp/Drawings/StarDrawing.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using WebApp.Model;
 
 namespace WebApp.Drawings
@@ -7,27 +8,67 @@ namespace WebApp.Drawings
 	{
         public void DrawOneStar(ITurtle t)
         {
-            double size = 10; 
-            double cx = 500; 
-            double cy = 300;
+            double startSize = 200; 
+            double startcx = 500; 
+            double startcy = 300;
+            double lines = 72;
+            double size = startSize;
+            double innerRadius = 10;
+            double turnAngle = 5;
+            (double cx, double cy) = Calculate_cx_cy(startcx, startcy, turnAngle, 0, innerRadius);
             // (center for stjernen er (cx,cy)
-            
+
             t.Up();
             t.MoveTo(cx, cy);
             t.TurnTo(0);
             t.Down();
-            t.Color = "blue";
-            t.Width = 1;
+            t.Width = 2;
            
-            for (int i = 0; i < 36; i++)
-            {           
+            for (int i = 0; i < lines; i++)
+            {
+                // Assign colours based on time tables
+                if (i % 7 == 0)
+                {
+                    t.Color = "pink";
+                }
+                else if (i % 3 == 0)
+                {
+                    t.Color = "red";
+                }
+                else if (i % 2 == 0)
+                {
+                    t.Color = "yellow";
+                } else 
+                {
+                    t.Color = "blue";
+                }
                 t.Move(size);
+                //size -= startSize / lines;
                 t.Up();
+                if (i % 2 == 0)
+                {
+                    (cx, cy) = Calculate_cx_cy(startcx, startcy, turnAngle, i, innerRadius);
+                } else
+                {
+                    (cx, cy) = Calculate_cx_cy(startcx, startcy, turnAngle, i, innerRadius+30);
+                }
+
                 t.MoveTo(cx, cy);
-                t.Turn(10);
+                t.Turn(turnAngle);
                 t.Down();
             }
+
+            static (double, double) Calculate_cx_cy (double startcx, double startcy, double turnAngle, int i, double innerRadius) 
+            {
+                double currentAngle = turnAngle * i;
+                double radians = Math.PI / 180 * currentAngle;
+                double cx = innerRadius * Math.Cos(radians) + startcx;
+                double cy = innerRadius * Math.Sin(radians) + startcy;
+                return (cx, cy);
+            }
         }
+
+
         
         /// <summary>
         /// Draw [n] stars of random size and location using
