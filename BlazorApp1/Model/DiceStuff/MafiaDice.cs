833a3e727cd57011f2ba22f14d521f5e1f4d80cd
AndreasDebel <andreasdebel@hotmail.com>
2024-10-23 16:09:07 +0200

    Færdig med alle opgaver fra modul 9. Det lykkedes ikke at få terningerne til at fylde en relativ del af siden.

--- BlazorApp1/Model/DiceStuff/MafiaDice.cs
+++ BlazorApp1/Model/DiceStuff/MafiaDice.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BlazorApp1.Model.DiceStuff;
+namespace BlazorApp1.Model.DiceStuff
 {
     public class MafiaDice : Dice
     {
